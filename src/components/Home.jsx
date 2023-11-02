eb5824417522557da7720115ddc55a775ef627ad
felipefraga <felifraga10@gmail.com>
2023-11-02 12:43:30 -0300

    segundo bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,9 @@ import { FaSpaceAwesome } from 'react-icons/fa6'
 
 export const Home = () => {
     return (
-        <div className="h-screen py-10 animate-fade-down ">
-            <div className='flex justify-between text-details dark:text-white h-[100%]'>
-                <div className='hidden sm:block'>
-{/*                     <StackUno />
-                    <StackTres /> */}
-                </div>
+        <div className="h-screen  py-10 animate-fade-down ">
+            <div className='flex justify-center text-details dark:text-white h-[100%]'>
+    
                 <div className='w-[80%] sm:text-right relative mx-10'>
                     <h1 className="text-4xl sm:text-7xl font-rosarivo">Fullstack <span className='text-detLight dark:text-details'> Frontend </span> <br></br> Developer<span className='text-twoLight dark:text-details'>.</span></h1>
 
@@ -19,25 +16,11 @@ export const Home = () => {
                         <Contacto />
                     </div>
 
-
                     <img src={astronauta} alt="Astronauta saludando" className='mt-[50px] sm:mt-0 h-[200px] sm:h-[250px] animate-wiggle-more animate-infinite animate-duration-[4111ms] ' />
 
-                    {/* <div className='block sm:hidden flex my-10'>
-                        <StackUno />
-                        <StackDos />
-                        <StackTres />
-                        <StackCuatro />
-                    </div> */}
-
-                        <a href="#proyectos"  className='absolute bottom-2 md:right-0 flex   transform rotate-180'>
-                            <FaSpaceAwesome className='text-detLight dark:text-details animate-bounce animate-infinite font-bold w-[35px] h-[35px]'/>
-                        </a>
-                </div>
-
-
-                <div className='hidden sm:block'>
-                    {/* <StackDos />
-                    <StackCuatro /> */}
+                    <a href="#proyectos" className='absolute bottom-2 md:right-0 flex   transform rotate-180'>
+                        <FaSpaceAwesome className='text-detLight dark:text-details animate-bounce animate-infinite font-bold w-[35px] h-[35px]' />
+                    </a>
                 </div>
             </div>
         </div>
